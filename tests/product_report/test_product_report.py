b2954455f15ddca5cf647d405d26a3fe26fb821b
lucas <mr.pine97@gmail.com>
2022-07-11 18:05:14 -0300

    req02 - feat: implementa a função test_relatorio_produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+
+
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Les Paul",
+        "Gibson",
+        "1952-01-01",
+        "2052-01-01",
+        "09031997",
+        "away from the sun",
+    )
+
+    assert (product.__repr__()) == (
+        f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa} com validade"
+        f" até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
